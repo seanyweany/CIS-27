9e30304fa7b310666d8d60c6d8c86b6149dc0e0c
Sean <seanaraitalley@gmail.com>
2024-02-02 11:56:02 -0800

    2/1/24

--- VSCode/playground/cis27Spring2024SeanTalleyPlayground.c
+++ VSCode/playground/cis27Spring2024SeanTalleyPlayground.c
@@ -1,29 +1,71 @@
-/*
+/**
  * Program Name: cis27Spring2024SeanTalleyPlayground.c
  * Discussion:   Mess 'round!
  * Written by:   Sean Talley
- * Date:         2024/01/23
+ * Date:         2024/02/01
  */
+
+#define TEST_1
+//#define TEST_2
 
 // Include and Header Files
 #include <stdio.h>
+#include <stdlib.h>
 
 // Function Prototypes
-void test(void);
+int* dang(void);
+void A(void);
+void B(void (*ptr)());
 
 // Application Driver
 int main() {
-    test();
+#ifdef TEST_1
+    int* iPtr = NULL;
+
+    iPtr = dang();
+
+    for (int i = 0; i < sizeof(iPtr); i++) {
+        printf("%d\n", *(iPtr + i));
+    }
+
+    free(iPtr);
+#endif
+
+#ifdef TEST_2
+    void (*ptr)() = &A;
+ 
+    // calling function B and passing
+    // address of the function A as argument
+    B(ptr);
+#endif
 
     return 0;
 }
 
 // Function Definitions
-void test() {
-    printf("rah!!\n");
+
+int* dang() {
+    int* iPtr = (int*)malloc(4 * sizeof(int));
+
+    for (int i = 0; i < sizeof(iPtr); i++) {
+        *(iPtr + i) = (i + 1) * (i + 1);
+    }
+
+    return iPtr;
 }
 
-/* OUTPUT
+void A() { 
+  printf("I am function A\n");
+}
+ 
+void B(void (*ptr)()) {
+    (*ptr)(); // callback to A
+}
+
+/** PROGRAM_OUTPUT
+
 */
 
-/* ISSUES */
+/* LOGIC_CODE_OUTPUT Issues
+None!
+ */
---
+++ VisualStudio/Quiz1/Quiz1/Quiz1.c
@@ -0,0 +1,53 @@
+﻿/**
+* Program Name : cis27Spring2024SeanTalleyPlayground.c
+* Discussion : Mess 'round!
+* Written by : Sean Talley
+* Date : 2024 / 02 / 01
+*/
+
+// #define _CRT_SECURE_NO_WARNINGS
+
+// Include and Header Files
+#include <stdio.h>
+
+// Function Prototypes
+void useArraySyntax01(void);
+void useArraySyntax02(void);
+
+// Application Driver
+int main() {
+
+    return 0;
+}
+
+// Function Definitions
+
+void useArraySyntax01() {
+    int arySize1;
+
+    printf("\nEnter an int for array size: ");
+    scanf("%d", &arySize1);
+
+    int ary2[arySize1];
+
+    printf("\narySize1 : %d\n", arySize1);
+
+    return;
+}
+
+void useArraySyntax02() {
+    int arySize2 = 5;
+    int ary2[arySize2];
+
+    printf("\narySize2 : %d\n", arySize2);
+
+    return;
+}
+
+/** PROGRAM_OUTPUT
+
+*/
+
+/* LOGIC_CODE_OUTPUT Issues
+None!
+ */

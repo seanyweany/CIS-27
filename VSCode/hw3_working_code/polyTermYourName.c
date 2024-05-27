b870cb07eb5e098f543e739613a666950a9101ce
Sean <seanaraitalley@gmail.com>
2024-05-27 02:54:38 +0000

    for pc

--- VSCode/hw3_working_code/polyTermYourName.c
+++ VSCode/hw3_working_code/polyTermYourName.c
@@ -1,28 +1,5 @@
 /**
- * Program Name: polyTermYourName.c
- * Discussion:   Implentation File
- *                 struct Fraction & Relevance
- * Written By:   Your Name
- * Date:         2024/__/__
- */
-
-#define _CRT_SECURE_NO_WARNINGS
-
-// Header/include File
-#include <stdio.h>
-#include <stdlib.h>
-#include "fractionYourName.h"
-#include "fractionUtilityYourName.h"
-#include "PolyTermYourName.h"
-
-TdPolyTermAddr createPolyTerm() {
-    int n;
-    int d;
-    TdPolyTermPtr polyTermPtr = NULL;
-
-    polyTermPtr = (TdPolyTermPtr)malloc(sizeof(TdPolyTerm));
-
-    printf("\nEnter a non-negative order for term: ");
+   ₿zzzzzzzzzzzzzzzzzzzzzzzzzz term: ");
     scanf("%d", &(polyTermPtr->order));
     
     printf("\nEnter an int for num: ");
